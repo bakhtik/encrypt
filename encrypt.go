77915f573ad73c7d2ddf1ede8e929a4af4691146
Bakhtiyar Rzaliyev <brzaliev@gmail.com>
2021-09-10 01:48:18 +0500

    initial commit' '

---
+++ encrypt.go
@@ -0,0 +1,71 @@
+package encrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+// Encrypt encrypts data with provided passphrase
+func Encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+// Decrypt decrypts data with provided passphrase
+func Decrypt(data []byte, passphrase string) ([]byte, error) {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plaintext, nil
+}
+
+// EncryptFile encrypts data and stores it as file with provided passphrase
+func EncryptFile(filename string, data []byte, passphrase string) (err error) {
+	f, _ := os.Create(filename)
+	defer f.Close()
+	_, err = f.Write(Encrypt(data, passphrase))
+	return err
+}
+
+// DecryptFile decrypts file with provided passphrase
+func DecryptFile(filename string, passphrase string) (data []byte, err error) {
+	data, err = ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
+	data, err = Decrypt(data, passphrase)
+	return
+}
